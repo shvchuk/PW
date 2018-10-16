4982154fae1ead63d1e6c09b0857d488ca64df76
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-10-16 17:16:05 +0200

    type writer effect & pictures

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,68 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
+// printer function
+const TypeWriter = function(txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+TypeWriter.prototype.type = function() {
+  // current index of the word
+  const current = this.wordIndex % this.words.length;
+  // get full text of current word
+  const fullTxt = this.words[current];
+  // check if deleting
+  if (this.isDeleting) {
+    // remove char
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // add char
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+  // insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // initial type speed
+  let typeSpeed = 300;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // if word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // make pause at end
+    typeSpeed = this.wait;
+    // set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    // move to the next word
+    this.wordIndex++;
+    // pause before start typing
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init on DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const txtElement = document.querySelector(".printer");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new TypeWriter(txtElement, words, wait);
+}
+
 // initial state of the menu
 let showMenu = false;
 
